fa2719b78a61a7f05826edf0903f71a244e392cc
pablomedrano <pablo.medrano@curantissolutions.com>
2022-02-28 21:32:15 -0600

    update person repository
    update

--- src/main/java/com/example/accessingdatarest/PersonRepository.java
+++ src/main/java/com/example/accessingdatarest/PersonRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
-	List<Person> findByLastName(@Param("lastname") String lastname);
+	List<Person> findByLastName(@Param("loquesea") String loquesea);
 
-	List<Person> findByFirstName(@Param("firstName") String firstName);
+	List<Person> findByFirstName(@Param("algomas") String algomas);
 
 }
